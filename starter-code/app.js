ca26f32ed747f7e0cbd1b6e387025667cb9e4559
Eduardo <ceciliorubes@gmail.com>
2020-02-01 00:08:31 +0100

    ejercicio terminado

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,16 +4,32 @@ const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
-const punkAPI = new PunkAPIWrapper();
+const punkAPI = new PunkAPIWrapper();;
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-// add the partials here:
-
-// add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beer => {
+      res.render('random-beer', { beer });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
